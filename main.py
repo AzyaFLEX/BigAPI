ba262a5deff2c1e5cd25f726ff193a30fb8b1bdd
g0lubew-iv <60041938+g0lubew-iv@users.noreply.github.com>
2021-02-08 18:34:57 +0300

    get_map 2.1
    Все элементы списка преобразуются к типу str

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 def get_map(coord: list, scale: float, type_map="map") -> requests.models.Response.content:
 	# Parameter type_map may be also "map" and "sat,skl"
 	api_server = "http://static-maps.yandex.ru/1.x/"
-	map_params = {"ll": ",".join(coord),
-				  "spn": ",".join([str(scale), str(scale)]),
-				  "l": type_map
+	map_params = {"ll": ",".join(list(map(str, coord))),
+		      "spn": ",".join([str(scale), str(scale)]),
+		      "l": type_map
 	}
 	response = requests.get(api_server, params=map_params)
 	return response.content
