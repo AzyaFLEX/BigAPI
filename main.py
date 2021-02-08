5fb5b6c67450125343110a3721203f92f2b32f6a
g0lubew-iv <60041938+g0lubew-iv@users.noreply.github.com>
2021-02-08 18:33:00 +0300

    get_map 2.0

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def get_map(coord: tuple, scale: float, type_map="map") -> requests.models.Response.content:
-    # Parameter type_map may be also "map" and "sat,skl"
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord[0]},{coord[1]}&spn={scale},{scale}&l={type_map}"
-    response = requests.get(map_request)
-
-    if not response:
-        return "Http статус:", response.status_code, "(", response.reason, ")"
-
-    return response.content
+def get_map(coord: list, scale: float, type_map="map") -> requests.models.Response.content:
+	# Parameter type_map may be also "map" and "sat,skl"
+	api_server = "http://static-maps.yandex.ru/1.x/"
+	map_params = {"ll": ",".join(coord),
+				  "spn": ",".join([str(scale), str(scale)]),
+				  "l": type_map
+	}
+	response = requests.get(api_server, params=map_params)
+	return response.content
