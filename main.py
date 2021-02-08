ccb78a962f6f0f896dd8c169cb80e9a08cb0dbc0
Chebekryak <tr.senya@yandex.ru>
2021-02-08 18:52:32 +0300

    get_index

--- main.py
+++ main.py
@@ -27,3 +27,20 @@ def get_address(address):
         return toponym_address
     else:
         return None
+
+
+def get_index(address):
+    address = (' ').join(address.split(", "))
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x" \
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        try:
+            postcode = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+        except KeyError:
+            postcode = None
+        return postcode
+    else:
+        return None
