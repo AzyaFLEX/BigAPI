7e1837f45226e8ea1bf57a0f68540451a5d437c5
g0lubew-iv <60041938+g0lubew-iv@users.noreply.github.com>
2021-02-08 18:20:11 +0300

    get_map 1.1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 
 
-def get_map(coord: tuple, scale: float, type_map="sat") -> requests.models.Response.content:
+def get_map(coord: tuple, scale: float, type_map="map") -> requests.models.Response.content:
+    # Parameter type_map may be also "map" and "sat,skl"
     map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord[0]},{coord[1]}&" \
-                  f"spn={scale},20&l={type_map}&geocode=Австралия "
+                  f"spn={scale},{scale}&l={type_map}&geocode=Австралия "
     response = requests.get(map_request)
 
     if not response:
