797b0f0839e665ef3512425cc7c9b275772c0d98
Chebekryak <tr.senya@yandex.ru>
2021-02-08 18:34:06 +0300

    fix

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import requests
 
 def get_coords(address):
     address = (' ').join(address.split(", "))
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apik" \
-                       f"ey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x"\
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
     response = requests.get(geocoder_request)
     if response:
         json_response = response.json()
         toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
         toponym_coodrinates = toponym["Point"]["pos"]
-        return list(map(float, toponym_coodrinates.split()))
+        return tuple(map(float, toponym_coodrinates.split()))
     else:
         return None
 
 
 def get_address(address):
     address = (' ').join(address.split(", "))
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apik" \
-                       f"ey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x"\
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
     response = requests.get(geocoder_request)
     if response:
         json_response = response.json()
