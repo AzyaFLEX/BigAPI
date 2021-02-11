47bea963e4433e718fa71552583fbcc0a8382406
Admiin <john20040409@gmail.com>
2021-02-11 18:53:48 +0300

    Задача 6. Метки сохраняются.

--- API.py
+++ API.py
@@ -2,39 +2,56 @@ import requests
 
 
 def get_map(coord: list, scale: float, type_map="map", pt=None) -> requests.models.Response.content:
-	"""A function that returns the map in bit form based on the received coordinates"""
-	# Parameter type_map may be also "map" and "sat,skl"
-	api_server = "http://static-maps.yandex.ru/1.x/"
-	map_params = {"ll": ",".join(list(map(str, coord))), "spn": ",".join([str(scale), str(scale)]), "l": type_map}
-	if pt:
-		map_params["pt"] = ",".join([str(pt[0]), str(pt[1]), "pmwtm1"])
-	response = requests.get(api_server, params=map_params)
-	return response.content
+    """A function that returns the map in bit form based on the received coordinates"""
+    # Parameter type_map may be also "map" and "sat,skl"
+    api_server = "http://static-maps.yandex.ru/1.x/"
+    map_params = {"ll": ",".join(list(map(str, coord))), "spn": ",".join([str(scale), str(scale)]), "l": type_map}
+    if pt:
+        map_params["pt"] = ",".join([str(pt[0]), str(pt[1]), "pmwtm1"])
+    response = requests.get(api_server, params=map_params)
+    return response.content
 
 
 def get_coords(address):
-	address = ' '.join(address.split(", "))
-	geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apik" \
-					   f"ey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
-	response = requests.get(geocoder_request)
-	if response:
-		json_response = response.json()
-		toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-		toponym_coodrinates = toponym["Point"]["pos"]
-		return tuple(map(float, toponym_coodrinates.split()))
-	else:
-		return None
+    address = (' ').join(address.split(", "))
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x"\
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        return tuple(map(float, toponym_coodrinates.split()))
+    else:
+        return None
 
 
 def get_address(address):
-	address = ' '.join(address.split(", "))
-	geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apik" \
-					   f"ey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
-	response = requests.get(geocoder_request)
-	if response:
-		json_response = response.json()
-		toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-		toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-		return toponym_address
-	else:
-		return None
+    address = (' ').join(address.split(", "))
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x"\
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        return toponym_address
+    else:
+        return None
+
+
+def get_index(address):
+    address = (' ').join(address.split(", "))
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x" \
+                       f"/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}, 1&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        try:
+            postcode = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+        except KeyError:
+            postcode = None
+        return postcode
+    else:
+        return None
--- main.py
+++ main.py
@@ -16,27 +16,71 @@ class App(QWidget):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.btn.clicked.connect(self.set_map)
+        self.change_lay.clicked.connect(self.def_change_lay)
+        self.return_lay.clicked.connect(self.def_return_lay)
+        self.cancel.clicked.connect(self.def_cancel)
         self.scale = 0.1
         self.start = "2-й Давыдовский мкр., 21, Кострома, Костромская обл., 156016"
-        self.coords = None
+        self.map = "map"
+        self.coords, self.coords_flag = None, None
         self.get_coords()
         self.set_map()
+
+    def def_cancel(self):
+        helper = self.field.text()
+        self.field.setText(self.start)
+        self.set_map()
+        self.field.setText(helper)
+
+    def def_change_lay(self):
+        self.map = {"map": "sat", "sat": "skl", "skl": "trf", "trf": "map"}[self.map]
+        self.update()
+
+    def def_return_lay(self):
+        self.map = "map"
+        self.update()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.scale + 0.1 <= 3:
+                self.scale += 0.1
+                self.update()
+        if event.key() == Qt.Key_PageDown:
+            if self.scale - 0.1 > 0.001:
+                self.scale -= 0.1
+                self.update()
+        move = self.scale / 0.1 * 0.01
+        if event.key() == Qt.Key_Up:
+            self.coords = (self.coords[0], self.coords[1] + move)
+            self.update()
+        if event.key() == Qt.Key_Left:
+            self.coords = (self.coords[0] - move, self.coords[1])
+            self.update()
+        if event.key() == Qt.Key_Right:
+            self.coords = (self.coords[0] + move, self.coords[1])
+            self.update()
+        if event.key() == Qt.Key_Down:
+            self.coords = (self.coords[0], self.coords[1] - move)
+            self.update()
 
     def set_map(self):
         try:
             self.get_coords()
             with open("map_file.txt", "wb") as file:
-                file.write(get_map(self.coords, self.scale, pt=self.coords))
+                self.coords_flag = self.coords
+                file.write(get_map(self.coords, self.scale, self.map, self.coords_flag))
             pixmap = QPixmap("map_file.txt")
             self.image.setPixmap(pixmap)
+            self.setFocus(True)
         except Exception as error:
             print(error)
 
     def update(self):
         with open("map_file.txt", "wb") as file:
-            file.write(get_map(self.coords, self.scale))
+            file.write(get_map(self.coords, self.scale, self.map, self.coords_flag))
         pixmap = QPixmap("map_file.txt")
         self.image.setPixmap(pixmap)
+        self.setFocus(True)
 
     def get_coords(self):
         if self.field.text():
